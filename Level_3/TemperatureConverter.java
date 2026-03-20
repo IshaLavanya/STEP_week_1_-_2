f7aa104c9249a86a6995997bff2250a30fd2bde3
Isha Lavanya <isha.lavanya07@gmail.com>
2026-03-21 00:00:22 +0530

    Added Level 3 Java programs

---
+++ Level_3/TemperatureConverter.java
@@ -0,0 +1,16 @@
+package Level_3;
+
+import java.util.Scanner;
+
+class TemperatureConverter {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter temperature in Celsius: ");
+            double celsius = input.nextDouble();
+
+            double fahrenheit = (celsius * 9 / 5) + 32;
+
+            System.out.println("Temperature in Fahrenheit = " + fahrenheit);
+        }
+    }
+}
